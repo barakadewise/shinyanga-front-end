95e041feca20343a150b3e69c897ee68b62e16c5
Baraka <barakadewise@gmail.com>
2024-05-24 17:21:21 +0300

    ui modification

--- utils/profile_context.py
+++ utils/profile_context.py
@@ -5,7 +5,6 @@ def user_profile_context_processor(request):
     api_service = ApiService()
     user_name = api_service.userProfileName(request)
     return {
-        'user_name': user_name,
-      
+        'user_name': user_name
     }
 
